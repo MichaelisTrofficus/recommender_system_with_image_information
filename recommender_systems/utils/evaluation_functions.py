396a5f64c002924c1d8d17f42e9c8d8a5b42d579
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2023-04-07 12:40:07 +0200

    Add posters and new notebooks

--- recommender_systems/utils/evaluation_functions.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def mae(u: np.ndarray, v: np.ndarray):
-    return np.nanmean(np.abs(u - v))
-
-
-def rmse(u: np.ndarray, v: np.ndarray):
-    return np.sqrt(np.nanmean(np.square(u - v)))
